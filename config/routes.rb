2b4b1719d405b8db2432d155b66d30dc8b8470c8
Szczurovsky <dszczurkowski@gmail.com>
2022-05-14 18:59:53 +0200

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
       resources :norgmars
       resources :postacs
     end
-    namespace :v3 do]
+    namespace :v3 do
       resources :klans
       resources :norgmars
       resources :postacs
