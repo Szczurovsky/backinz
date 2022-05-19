f72b82c3d919dec65d0579ab6602396ae5c36a55
Szczurovsky <dszczurkowski@gmail.com>
2022-05-19 14:01:08 +0200

    aaa

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,16 +17,16 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000"
     resource "*", header: :any, methods:[:get, :post, :put, :patch, :delete, :options, :head],
-    withCredentials: false
+    Credentials: true
   end
   allow do
     origins "https://inzynierkatest.herokuapp.com/"
     resource "*", header: :any, methods:[:get, :post, :put, :patch, :delete, :options, :head],
-    withCredentials: false
+    Credentials: true
   end
   allow do
     origins "https://inzynierka-test.herokuapp.com/"
     resource "*", header: :any, methods:[:get, :post, :put, :patch, :delete, :options, :head],
-    withCredentials: false
+    Credentials: true
   end
 end
