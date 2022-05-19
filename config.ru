65fd35acf736ae21451707e34b49d4c4a3ff0318
Szczurovsky <dszczurkowski@gmail.com>
2022-05-19 14:44:10 +0200

    help

--- config.ru
+++ config.ru
@@ -5,15 +5,7 @@
 
 require_relative "config/environment"
 
-use Rack::Cors do
-    allow do
-      origins 'https://inzynierka-test.herokuapp.com/'
-      resource '*',
-          :headers => :any,
-          :methods => [:get, :post, :delete, :put, :patch, :options],
-          withCredentials: true
-    end
-end
+
 
 run Rails.application
 Rails.application.load_server
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -19,11 +19,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*", headers: :any, methods:[:get, :post, :put, :patch, :delete, :options, :head],
     withCredentials: true
   end
-  allow do
-    origins "https://inzynierkatest.herokuapp.com/"
-    resource "*", headers: :any, methods:[:get, :post, :put, :patch, :delete, :options, :head],
-    withCredentials: true
-  end
+  # allow do
+  #   origins "https://inzynierkatest.herokuapp.com/"
+  #   resource "*", headers: :any, methods:[:get, :post, :put, :patch, :delete, :options, :head],
+  #   withCredentials: true
+  # end
   allow do
     origins "https://inzynierka-test.herokuapp.com/"
     resource "*", headers: :any, methods:[:get, :post, :put, :patch, :delete, :options, :head],
