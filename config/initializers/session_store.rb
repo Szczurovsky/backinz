3ab2e1ba878b1ec3f338ed43860718982e852035
Szczurovsky <dszczurkowski@gmail.com>
2022-05-22 23:10:00 +0200

    dopisek apiv3 do domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
     if Rails.env == "production"
-        Rails.application.config.session_store :cookie_store, key: "_auth_app", domain: "inzynierkatest.herokuapp.com"
+        Rails.application.config.session_store :cookie_store, key: "_auth_app", domain: "inzynierkatest.herokuapp.com/api/v3/"
     else
         Rails.application.config.session_store :cookie_store, key: "_auth_app"
 
