d67ad399ed69fe306f7765925e7dc9239bb5c22f
Szczurovsky <dszczurkowski@gmail.com>
2022-05-22 23:29:51 +0200

    do cofnieciea epwnie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base 
    include ActionController::Cookies
    skip_before_action :verify_authenticity_token
+
+   before_action :set_csrf_cookie
+  
+    include ActionController::RequestForgeryProtection
+  
+    protect_from_forgery with: :exception 
+		
+    def cookie 
+        "ok"
+    end
+		
+    private 
+		
+    def set_csrf_cookie
+       cookies["CSRF-TOKEN"] = {
+            value: form_authenticity_token,
+            domain: :all 
+        }
+    end
 end
