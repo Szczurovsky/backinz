5c8a42dac11bb60e7055733118ebd17c65706b18
Szczurovsky <dszczurkowski@gmail.com>
2022-05-23 00:35:28 +0200

    maybe baybe?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
-    if Rails.env == "production"
-        Rails.application.config.session_store :cache_store, key: "_auth_app", domain: "inzynierkatest.herokuapp.com", httponly: false
+    if Rails.env.production?
+        Rails.application.config.session_store :cookie_store, :key => "_auth_app", domain: :all, httponly: false, tld_length: 2
     else
-        Rails.application.config.session_store :cache_store, key: "_auth_app", httponly: false
+        Rails.application.config.session_store :cookie_store, key: "_auth_app", httponly: false
 
     end
