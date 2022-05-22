430a860ed1e63bae05e0077d4e8ba7af6e63ff74
Szczurovsky <dszczurkowski@gmail.com>
2022-05-23 00:19:01 +0200

    cache?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
     if Rails.env == "production"
-        Rails.application.config.session_store :cookie_store, key: "_auth_app", domain: "inzynierkatest.herokuapp.com", httponly: false
+        Rails.application.config.cache_store :cookie_store, key: "_auth_app", domain: "inzynierkatest.herokuapp.com", httponly: false
     else
-        Rails.application.config.session_store :cookie_store, key: "_auth_app", httponly: false
+        Rails.application.config.cache_store :cookie_store, key: "_auth_app", httponly: false
 
     end
