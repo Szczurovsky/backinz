244cc624cf1912550c41ed69906311cff9f1da65
Szczurovsky <dszczurkowski@gmail.com>
2022-05-22 20:30:21 +0200

    change session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env =="production"
-Rails.application.config.session_store :cookie_store, key: "_back", domain:"inzynierkatest.herokuapp.com"
+Rails.application.config.session_store :cookie_store, key: "_interslice_session", domain:"inzynierkatest.herokuapp.com"
 else
-    Rails.application.config.session_store :cookie_store, key: "_back"
+    Rails.application.config.session_store :cookie_store, key: "_interslice_session"
 end
