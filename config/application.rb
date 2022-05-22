202441a59574aeeb8c0a7a428132fa304ae23b6d
Szczurovsky <dszczurkowski@gmail.com>
2022-05-23 00:23:41 +0200

    a

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module Back
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.use ActionDispatch::Session::CookieStore, key: "_auth_app"
+    config.middleware.use ActionDispatch::Session::CacheStore, key: "_auth_app"
     config.middleware.use ActionDispatch::Cookies
   end
 end
